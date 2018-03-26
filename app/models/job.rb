f7d89139ec2a2bd146748294eeeadf922408c608
shichengyuan <15036062755@163.com>
2018-03-26 16:23:36 +0800

    implement wage part/

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,5 @@ class Job < ApplicationRecord
 
   validates :wage_lower_bound, presence:true
   validates :wage_upper_bound, presence:true
-  validates :contact_email, numericality: { greater_than; 0}
+  validates :wage_lower_bound, numericality: { greater_than; 0}
 end
