10df6c79ec8b0e7b8efeec47dec8e30867ec1271
shichengyuan <15036062755@163.com>
2018-03-27 14:25:48 +0800

    step7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 
 namespace :admin do
-  resources :jobs
-end 
+  resources :jobs do
+    member do
+      post :publish
+      post :hide
+    end
+  end
+end
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
 
 
